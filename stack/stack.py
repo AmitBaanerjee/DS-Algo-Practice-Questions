4eaa9d44703edec46df5d16f0893a916c2396c35
Amit Banerjee <amit_b@live.com>
2019-04-17 03:01:36 -0400

    adding stacks and queues

---
+++ stack/stack.py
@@ -0,0 +1,43 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+class Stack(object):
+    def __init__(self):
+        self.items=[]
+    def push(self,value):
+        self.items.append(value)
+    def pop(self):
+        return self.items.pop()
+    def peek(self):
+        return self.items[-1]
+    def size(self):
+        return len(self.items)
+    def isEmpty(self):
+        return len(self.items)==0
+    def printElements(self):
+        if self.isEmpty():
+            return None
+        else:
+            for i in self.items:
+                print(i)
+        
+        
+
+s=Stack()
+s.push(2)
+s.push(3)
+s.peek()
+s.pop()
+s.printElements()
+s.push(90)
+s.printElements()
+
+
+# In[3]:
+
+
+
+
