110bdc58bef08e12cf4ccfff90cf85967ae224df
Amit Banerjee <amit_b@live.com>
2019-06-17 19:22:25 -0400

    restructing inheritance folder

---
+++ practise problems/Java Practise/Inheritance/Dog.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public abstract class Dog extends Animal{
+	public Dog() {
+		System.out.println("A new dog is getting created inside Dog constructor!");
+	}
+	
+	public abstract void bark();
+	
+	
+}
