9a3baeadf01df85823a8cd2ee4bdabcc4b84c2b0
Amit Banerjee <amit_b@live.com>
2019-07-05 13:59:53 -0400

    corrected the bubble sort program

--- practise problems/Java Practise/bubbleSort.java
+++ practise problems/Java Practise/bubbleSort.java
@@ -1,37 +1,24 @@
-//BUBBLE SORT IN JAVA
+//Performing bubble sort in java according to geekforgeeks algorithm explanation
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Arrays;
 
-public class Solution {
-    public static void bubbleSort(int[] array){
-        int length=array.length;
-        int numSwaps=0;
-        int temp=0;
-        for(int i=0;i<length;i++){
-            for(int j=0;j<length-1;j++){
-                if (array[j]>array[j+1]){
-                    temp=array[j];
-                    array[j]=array[j+1];
-                    array[j+1]=temp;
-                    numSwaps+=1;
-                }
-            }
-        }
-        System.out.println("Array is sorted in "+numSwaps+" swaps.");
-        System.out.println("First Element: "+array[0]);
-        System.out.println("Last Element: "+array[array.length-1]);
-    }
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[] a = new int[n];
-        for(int a_i=0; a_i < n; a_i++){
-            a[a_i] = in.nextInt();
-        }
-        bubbleSort(a);
-    }
+public class bubbleSort {
+	public static void main(String args[]) {
+		System.out.println("Practising the bubble sort program in java...");
+		int[] array= {5,4,3,2,1};
+		int arrayLength=array.length;
+		int numberOfShuffles=0;
+		for(int i=0;i<arrayLength;i++) {
+			for(int j=0;j<arrayLength-i-1;j++) {
+				if(array[j]>array[j+1]) {
+					int temp=array[j];
+					array[j]=array[j+1];
+					array[j+1]=temp;
+					numberOfShuffles++;
+				}
+			}
+			System.out.println("The arrays looks like this after iteration: "+i + "-->"+ Arrays.toString(array));
+		}
+		System.out.println("Bubble sort was performed with "+numberOfShuffles+ " number of shuffles");
+	}
 }
