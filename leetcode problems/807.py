db0f119b7013971ca29c126d417456ef58691484
Amit Banerjee <amit_b@live.com>
2019-08-29 11:35:05 -0400

    807.Max Increase to Keep City Skyline

---
+++ leetcode problems/807.py
@@ -0,0 +1,47 @@
+# 807. Max Increase to Keep City Skyline
+#
+# In a 2 dimensional array grid, each value grid[i][j] represents the height of a building located there. We are allowed to increase the height of any number of buildings, by any amount (the amounts can be different for different buildings). Height 0 is considered to be a building as well.
+#
+# At the end, the "skyline" when viewed from all four directions of the grid, i.e. top, bottom, left, and right, must be the same as the skyline of the original grid. A city's skyline is the outer contour of the rectangles formed by all the buildings when viewed from a distance. See the following example.
+#
+# What is the maximum total sum that the height of the buildings can be increased?
+#
+# Example:
+# Input: grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
+# Output: 35
+# Explanation:
+# The grid is:
+# [ [3, 0, 8, 4],
+#   [2, 4, 5, 7],
+#   [9, 2, 6, 3],
+#   [0, 3, 1, 0] ]
+#
+# The skyline viewed from top or bottom is: [9, 4, 8, 7]
+# The skyline viewed from left or right is: [8, 7, 9, 3]
+#
+# The grid after increasing the height of buildings without affecting skylines is:
+#
+# gridNew = [ [8, 4, 8, 7],
+#             [7, 4, 7, 7],
+#             [9, 4, 8, 7],
+#             [3, 3, 3, 3] ]
+
+
+class Solution:
+    def checkSkyline(self,i,j,grid):
+        #check row
+        maxr=max(grid[i])
+        #check column
+        maxc=0
+        for i in range(len(grid)):
+            if grid[i][j]>maxc:
+                maxc=grid[i][j]
+        return min(maxc,maxr)
+    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
+        change=0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                minval=self.checkSkyline(i,j,grid)
+                change+=minval-grid[i][j]
+                grid[i][j]=minval
+            return change
