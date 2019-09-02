24c5b983429cac7678f277b731763aadc83b28b8
Amit Banerjee <amit_b@live.com>
2019-09-01 20:42:59 -0400

    661.Image Smoother

---
+++ leetcode problems/661.py
@@ -0,0 +1,40 @@
+# 661. Image Smoother
+#
+# Given a 2D integer matrix M representing the gray scale of an image, you need to design a smoother to make the gray scale of each cell becomes the average gray scale (rounding down) of all the 8 surrounding cells and itself. If a cell has less than 8 surrounding cells, then use as many as you can.
+#
+# Example 1:
+#
+# Input:
+# [[1,1,1],
+#  [1,0,1],
+#  [1,1,1]]
+# Output:
+# [[0, 0, 0],
+#  [0, 0, 0],
+#  [0, 0, 0]]
+# Explanation:
+# For the point (0,0), (0,2), (2,0), (2,2): floor(3/4) = floor(0.75) = 0
+# For the point (0,1), (1,0), (1,2), (2,1): floor(5/6) = floor(0.83333333) = 0
+# For the point (1,1): floor(8/9) = floor(0.88888889) = 0
+
+class Solution(object):
+    def imageSmoother(self, M):
+        """
+        :type M: List[List[int]]
+        :rtype: List[List[int]]
+        """
+
+        R, C = len(M), len(M[0])
+        ans = [[0] * C for _ in M]
+
+        for r in xrange(R):
+            for c in xrange(C):
+                count = 0
+                for nr in (r-1, r, r+1):
+                    for nc in (c-1, c, c+1):
+                        if 0 <= nr < R and 0 <= nc < C:
+                            ans[r][c] += M[nr][nc]
+                            count += 1
+                ans[r][c] /= count
+
+        return ans
