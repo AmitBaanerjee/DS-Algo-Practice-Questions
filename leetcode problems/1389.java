0c186b3b901cf6dc3fe1be222c6aed25135e58eb
Amit Banerjee <40642807+AmitBaanerjee@users.noreply.github.com>
2020-03-30 13:36:01 -0700

    1389. Create Target Array in the Given Order

---
+++ leetcode problems/1389.java
@@ -0,0 +1,66 @@
+1389. Create Target Array in the Given Order
+
+Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+Initially target array is empty.
+From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+Repeat the previous step until there are no elements to read in nums and index.
+Return the target array.
+
+It is guaranteed that the insertion operations will be valid.
+
+Example 1:
+
+Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+Output: [0,4,1,3,2]
+Explanation:
+nums       index     target
+0            0        [0]
+1            1        [0,1]
+2            2        [0,1,2]
+3            2        [0,1,3,2]
+4            1        [0,4,1,3,2]
+Example 2:
+
+Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
+Output: [0,1,2,3,4]
+Explanation:
+nums       index     target
+1            0        [1]
+2            1        [1,2]
+3            2        [1,2,3]
+4            3        [1,2,3,4]
+0            0        [0,1,2,3,4]
+Example 3:
+
+Input: nums = [1], index = [0]
+Output: [1]
+
+
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        
+        int[] target=new int[nums.length];
+        for(int i=0;i<target.length;i++){
+            target[i]=-5;
+        }
+        // Arrays.fill(target,-5);
+        int previous,temp,ind,targetValue;
+
+        for (int i=0;i<nums.length;i++){
+            ind=index[i];
+            targetValue=nums[i];
+            if(target[ind]!=-5){
+                previous=target[ind];
+                for(int j=ind+1;j<nums.length;j++){
+                    temp=target[j];
+                    target[j]=previous;
+                    previous=temp;
+                }
+            }
+            target[ind]=targetValue;
+
+        }
+        return target;
+    }
+}
